6db54aaecfa438ce1356b4d6ee0f2d81bcadefd9
Andrew Snyder <ASnyder@minitab.com>
2022-07-27 09:22:37 -0400

    just need a LoadBalancer

--- repro.sh
+++ repro.sh
@@ -23,22 +23,13 @@ az aks create \
   --uptime-sla \
   --enable-encryption-at-host
 
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-helm upgrade --install nginx ingress-nginx/ingress-nginx \
-  --create-namespace \
-  --namespace ingress \
-  --set controller.replicaCount=0 \
-  --set controller.service.externalTrafficPolicy=Local \
-  --wait
-
 az aks get-credentials --name $aks --resource-group $rg
 
 kubectl create namespace api-issue
 
 # add kubectl repro workload
 # should show error as kubectl client timing out after 10 seconds trying to TLS handshake
-kubectl apply -f kubectl.yaml
+kubectl apply -n api-issue -f kubectl.yaml
 
 # build simple nodejs example that uses k8s sdk
 # node failures will detect any api request longer than 60 seconds and exit the pod
@@ -47,7 +38,26 @@ az acr login --name $acr
 IMAGE=$acr.azurecr.io/slowrequest-node:latest
 docker build -t $IMAGE .
 docker push $IMAGE
-sed "s/{acr}/$acr/g" node.yaml | kubectl apply -f - 
+sed "s/{acr}/$acr/g" node.yaml | kubectl apply -n api-issue -f - 
+
+# at this point these jobs will run fine, i have run them for days without issue. but the next step will start to cause intermittent issues 
+
+# does not need to point at anything all that seems required is that a public ingress ip address is created
+# even if this points to something valid it make no difference
+# intermittent issues in the jobs created above will start appearing
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: my-service
+  namespace: api-issue
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 80
+  selector:
+    app: myapp
+EOF
 
 # wait for failures
 kubectl get pods -n api-issue -w
